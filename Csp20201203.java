84293260efd8c0372410cb8ba7b0b05ec4705f08
Jenneyc <115560046+Jenneyc@users.noreply.github.com>
2022-11-11 20:57:38 +0800

    Add files via upload

--- Csp20201203.java
+++ Csp20201203.java
@@ -1,120 +1,164 @@
 package sheep;
 import java.util.*;
 
-class file{
+class node{
 	boolean isfile;
-	long limitall;
-	long limitdir;
 	long size;
-	long nextsize;
-	long allsize;
-	boolean allchange=false;
-	boolean nextchange=false;
-	Map<String,file> child;
-	public file(boolean isfile,long size) {
-		this.isfile=isfile;
+	long ld;
+	long lr;
+	long dd;
+	long rr;
+	HashMap<String,node> child;
+	boolean ddchange;
+	boolean rrchange;
+	public node(boolean isfile,long size) {
 		if(!isfile) {
+			this.isfile=isfile;
+			this.size=size;
+			ld=0;
+			lr=0;
 			child=new HashMap<>();
-			limitall=0;
-			limitdir=0;
+			ddchange=false;
+			rrchange=false;
 		}
 		else {
+			this.isfile=isfile;
 			this.size=size;
 		}
 	}
-	public file findfile(String[] split,int cur) {
-		if(cur==split.length) {
-			return this;
-		}
-		if(!isfile&&child.containsKey(split[cur])) {
-			return child.get(split[cur]).findfile(split, cur+1);
+}
+
+class tree{
+	node root;
+	public tree() {
+		root=new node(false,0);
+	}
+	public node findnode(node p,String[] path,int cur) {
+		if(cur==path.length) {
+			return p;
 		}
 		else {
-			return null;
-		}
-	}
-	public long getnextsize() {
-		if(!nextchange) {
-			return nextsize;
-		}
-		long ans=0;
-		for(file f:child.values()) {
-			ans+=f.size;
-		}
-		nextsize=ans;
-		nextchange=false;
-		return ans;
-	}
-	public long getallsize() {
-		if(!allchange) {
-			return allsize;
-		}
-		long ans=0;
-		for(file f:child.values()) {
-			if(f.isfile) {
-				ans+=f.size;
+			if(!p.isfile&&p.child.containsKey(path[cur])) {
+				return findnode(p.child.get(path[cur]),path,cur+1);
 			}
 			else {
-				ans+=f.getallsize();
+				return null;
 			}
 		}
-		allsize=ans;
-		allchange=false;
-		return ans;
 	}
-	public boolean createfile(String[] path,long size,int cur) {
-		if(cur+1==path.length) {
-			if((limitall!=0&&getallsize()+size>limitall)||
-					(limitdir!=0&&getnextsize()+size>limitdir)) {
-				return false;
-			}
-			file next=child.get(path[cur]);
-			if(next==null) {
-				next=new file(true,size);
-				child.put(path[cur], next);
-			}
-			else {
-				next.size+=size;
-			}
-			allchange=true;
-			nextchange=true;
-			return true;
+	public long getrr(node p) {
+		if(!p.rrchange) {
+			return p.rr;
 		}
 		else {
-			if(limitall!=0&&getallsize()+size>limitall) {
-				return false;
-			}
-			file next=child.get(path[cur]);
-			if(next==null) {
-				next=new file(false,0);
-				child.put(path[cur], next);
-			}
-			if(next.isfile) {
-				return false;
-			}
-			else {
-				if(next.createfile(path, size, cur+1)) {
-					allchange=true;
-					return true;
+			long ans=0;
+			for(node a:p.child.values()) {
+				if(a.isfile) {
+					ans+=a.size;
 				}
-				return false;
+				else {
+					ans+=getrr(a);
+				}
 			}
+			p.rr=ans;
+			p.rrchange=false;
+			return ans;
 		}
 	}
-	public boolean removefile(String[] path,int cur) {
-		if(cur+1==path.length) {
-			nextchange=true;
-			allchange=true;
-			child.remove(path[cur]);
-			return true;
+	public long getdd(node p) {
+		if(!p.ddchange) {
+			return p.dd;
 		}
 		else {
-			file next=child.get(path[cur]);
-			if(next!=null&&!next.isfile&&next.removefile(path, cur+1)) {
-				allchange=true;
+			long ans=0;
+			for(node a:p.child.values()) {
+					ans+=a.size;
+			}
+			p.dd=ans;
+			p.ddchange=false;
+			return p.dd;
+		}
+	}
+	public boolean createnode(node p,String[] path,long size,int cur) {
+		if(cur+1==path.length) {
+			if(
+					(p.lr!=0&&getrr(p)+size>p.lr)||
+					(p.ld!=0&&getdd(p)+size>p.ld)) {
+				return false;
+			}
+			else {
+				node t=p.child.get(path[cur]);
+				if(t==null) {
+					t=new node(true,size);
+					p.child.put(path[cur], t);
+				}
+				else if(t.isfile) {
+					t.size+=size;
+				}
+				p.ddchange=true;
+				p.rrchange=true;
 				return true;
 			}
-			return false;
+		}
+		else {
+			if(p.isfile||
+					(p.lr!=0&&getrr(p)+size>p.lr)) {
+				return false;
+			}
+			else {
+				node t=p.child.get(path[cur]);
+				if(t==null) {
+					t=new node(false,0);
+					p.child.put(path[cur], t);
+				}
+				if(t.isfile) {
+					return false;
+				}
+				else {
+					if(createnode(t,path,size,cur+1)) {
+						p.rrchange=true;
+						return true;
+					}
+					return false;
+				}
+			}
+		}
+	}
+	public boolean remove(node p,String[] path,int cur) {
+		if(cur+1==path.length) {
+			if(p.isfile) {
+				return false;
+			}
+			node t=p.child.get(path[cur]);
+			if(t!=null) {
+				p.child.remove(path[cur]);
+				p.ddchange=true;
+				p.rrchange=true;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			if(p.isfile) {
+				return false;
+			}
+			else {
+				node t=p.child.get(path[cur]);
+				if(t==null) {
+					return false;
+				}
+				else {
+					if(remove(t,path,cur+1)) {
+						p.rrchange=true;
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			}
 		}
 	}
 }
@@ -123,62 +167,65 @@ public class Csp20201203 {
   public static void main(String[] args) {
 	  Scanner s=new Scanner(System.in);
 	  int t=s.nextInt();
-	  s.nextLine();
-	  String op;
-	  file root=new file(false,0);
+	  tree mytree=new tree();
 	  while(t--!=0) {
-		  op=s.nextLine();
-		  String[] ops=op.split(" ");
+		  char op=s.next().charAt(0);
+		  String a=s.next();
 		  boolean result=false;
-		  String[] path;
-		  switch(ops[0]) {
-		  case"C":
-			  path=ops[1].substring(1).split("/");
-			  long size=Long.parseLong(ops[2]);
-			  file f=root.findfile(path,0);
-			  if(f!=null) {
-				  if(f.isfile) {
-					  long changesize=size-f.size;
-					  if(root.createfile(path,changesize,0)) {
-						  result=true;
-					  }
+		  node p;
+		  String[] path=a.substring(1).split("/");
+		  switch(op) {
+		  case'C':
+			  long size=s.nextLong();
+			  p=mytree.findnode(mytree.root, path, 0);
+			  if(p!=null) {
+				  if(p.isfile) {
+					  size=size-p.size;
+					  result=mytree.createnode(mytree.root,path,size,0);
+				  }
+				  else {
+					  result=false;
 				  }
 			  }
 			  else {
-				  if(root.createfile(path,size,0)) {
-					  result=true;
-				  }
+				  result=mytree.createnode(mytree.root,path,size,0);
 			  }
 			  break;
-		  case"R":
-			  path=ops[1].substring(1).split("/");
-			  root.removefile(path,0);
+		  case'R':
+			  mytree.remove(mytree.root,path,0);
 			  result=true;
 			  break;
-		  case"Q":
-			  long ld=Long.parseLong(ops[2]);
-			  long lr=Long.parseLong(ops[3]);
-			  path=ops[1].substring(1).split("/");
-			  file target=root.findfile(path, 0);
-			  if(path[0].equals("")){
-				  target=root;
+		  case'Q':
+			  long ld=s.nextLong();
+			  long lr=s.nextLong();
+			  node f=mytree.findnode(mytree.root, path, 0);
+			  if(path[0].equals("")) {
+				  f=mytree.root;
 			  }
-			  if(target!=null&&!target.isfile) {
-				  if((lr==0||target.getallsize()<=lr)
-						  &&(ld==0||target.getnextsize()<=ld)) {
-					  target.limitall=lr;
-					  target.limitdir=ld;
+			  if(f==null) {
+				  result=false;
+			  }
+			  else if(f.isfile) {
+				  result=false;
+			  }
+			  else {
+				  if(ld!=0&&ld<mytree.getdd(f)||
+						  lr!=0&&mytree.getrr(f)>lr) {
+					  result=false;
+				  }
+				  else {
+					  f.ld=ld;
+					  f.lr=lr;
 					  result=true;
 				  }
 			  }
 			  break;
 		  }
+		  char r='N';
 		  if(result) {
-			  System.out.println("Y");
+			  r='Y';
 		  }
-		  else {
-			  System.out.println("N");
-		  }
+		  System.out.println(r);
 	  }
   }
 }
